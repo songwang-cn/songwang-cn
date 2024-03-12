d12c87df3db7023149736ab884c42f45d01c11b2
宋旺 <songwang2209@163.com>
2024-03-12 11:54:00 +0800

    'Greate'

--- littleGreen.bat
+++ littleGreen.bat
@@ -13,8 +13,15 @@ for /l %%i in (1, 1, %count%) do (
 )
 echo *----------------SUCCESS--------------------*
 echo *                                           *
-echo *                a commit a day             *
-echo *         keeps your girlfriend away        *
+echo *             a commit a day                *
+echo *        keeps your girlfriend away         *
 echo *                                           *
+echo *        _ _ _          _   _     _         * 
+echo *       | (_) | _____  | |_| |__ (_)___     * 
+echo *       | | | |/ / _ \ | __| '_ \| / __|    * 
+echo *       | | |   <  __/ | |_| | | | \__ \    * 
+echo *       |_|_|_|\_\___|  \__|_| |_|_|___/    * 	
+echo *                                           *
+echo *                                           *				
 echo *-----------------SUCCESS-------------------*
 timeout /nobreak /t 10
