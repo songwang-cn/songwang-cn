ab1d5acf2552f16c290f00e144d46213df90adc3
宋旺 <songwang2209@163.com>
2024-03-12 11:59:00 +0800

    'Greate'

--- littleGreen.bat
+++ littleGreen.bat
@@ -1,8 +1,8 @@
 @echo off
 git pull
 ::随机生成12-（12+18-1）的随机数count%%(end-1)+start
-set count=%random%
-set /a count=count%%18+12
+set count=3
+::set /a count=count%%18+12
 echo total:%count%
 for /l %%i in (1, 1, %count%) do (
 	echo var a = [%%i] > index.js
@@ -15,13 +15,6 @@ echo *----------------SUCCESS--------------------*
 echo *                                           *
 echo *             a commit a day                *
 echo *        keeps your girlfriend away         *
-echo *                                           *
-echo *        _ _ _          _   _     _         * 
-echo *       | (_) | _____  | |_| |__ (_)___     * 
-echo *       | | | |/ / _ \ | __| '_ \| / __|    * 
-echo *       | | |   <  __/ | |_| | | | \__ \    * 
-echo *       |_|_|_|\_\___|  \__|_| |_|_|___/    * 	
-echo *                                           *
-echo *                                           *				
+echo *                                           *			
 echo *-----------------SUCCESS-------------------*
 timeout /nobreak /t 10
